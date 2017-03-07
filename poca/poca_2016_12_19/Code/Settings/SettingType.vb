9511d023195d583117930c0e75a895a3375e1d58
arkabiswas <arkarainmaker@gmail.com>
2017-03-06 19:25:00 -0500

    poca remove

--- poca/poca_2016_12_19/Code/Settings/SettingType.vb
+++
@@ -1,9 +0,0 @@
-' Static Model
-Namespace FDA.BusinessObjects
-    Public Class SettingType
-
-        Public SettingName As String
-        Public SettingValue As String
-
-    End Class ' END CLASS DEFINITION SettingType
-End Namespace
